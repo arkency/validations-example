7b26fdc083b301bbb15828b790beb97d739148e5
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-12-07 00:04:27 +0100

    Up–to–date tests

--- test/controllers/signup_controller_test.rb
+++ test/controllers/signup_controller_test.rb
@@ -1,13 +1,21 @@
 require "test_helper"
 
 class SignupControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_new_url
+  test "should render signup form with no issues" do
+    get signups_path
+
     assert_response :success
   end
 
-  test "should get create" do
-    get signup_create_url
-    assert_response :success
+  test "should redirect after successful creation" do
+    post signups_path, params: { signup: { email: "joe@example.com", password: "password" } }
+
+    assert_redirected_to signups_path
+  end
+
+  test " should render signup form with errors" do
+    post signups_path, params: { signup: { email: "joe@example.com", password: "pass" } }
+
+    assert_response :unprocessable_entity
   end
 end
